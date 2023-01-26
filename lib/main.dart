30d762e32d4965dde695b6dc14f3dba7e6a57af8
danaFlorescu <mariadana977@gmail.com>
2023-01-26 14:46:11 +0200

    added more cats img and sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+//Illustration by Ekaterina Rogova from Ouch!
+//Illustration by Icons 8 from Ouch!
+
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
@@ -29,13 +32,96 @@ class CatApp extends StatelessWidget {
                         AssetSource('angry.wav'),
                       );
                     },
-                    child: Image.asset('images/kitekat-13.png'),
+                    child: Image.asset('images/angry.png'),
                   ),
                 ),
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset('images/kitekat-13.png'),
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(
+                        AssetSource('purring.wav'),
+                      );
+                    },
+                    child: Image.asset('images/purring.png'),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(
+                        AssetSource('short_meow.wav'),
+                      );
+                    },
+                    child: Image.asset('images/short_meow.png'),
+                  ),
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(
+                        AssetSource('screaming_cat.wav'),
+                      );
+                    },
+                    child: Image.asset('images/screaming_cat.png'),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(
+                        AssetSource('figthing.wav'),
+                      );
+                    },
+                    child: Image.asset('images/figthing.png'),
+                  ),
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(
+                        AssetSource('surprised.wav'),
+                      );
+                    },
+                    child: Image.asset('images/surprised.png'),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(
+                        AssetSource('upset.wav'),
+                      );
+                    },
+                    child: Image.asset('images/upset.png'),
+                  ),
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(
+                        AssetSource('cat_food.wav'),
+                      );
+                    },
+                    child: Image.asset('images/cat_food.png'),
                   ),
                 ),
               ],
