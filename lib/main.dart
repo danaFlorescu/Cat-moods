b44f09cdc9260e6340b3627feb0e63b41dda3ffa
danaFlorescu <mariadana977@gmail.com>
2023-01-30 15:44:21 +0200

    modifed audioplayer - only one sound at the time + fixed the puzzled cat audio

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,45 @@ import 'package:audioplayers/audioplayers.dart';
 import 'constans.dart';
 
 void main() => runApp(
-      const CatApp(),
+      CatApp(),
     );
 
 class CatApp extends StatelessWidget {
   const CatApp({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CatAppClass(),
+    );
+  }
+}
+
+class CatAppClass extends StatefulWidget {
+  const CatAppClass({Key? key}) : super(key: key);
+
+  @override
+  State<CatAppClass> createState() => _CatAppClassState();
+}
+
+class _CatAppClassState extends State<CatAppClass> {
+  AudioPlayer player = AudioPlayer();
+
   void playSound(String nameOfFile) {
-    final player = AudioPlayer();
     player.play(
       AssetSource(nameOfFile),
     );
   }
 
-  Expanded buildCat(String catMood, Color color) {
+// 1 - play, 2-stop
+  Expanded buildCat(String catMood, Color color, String extension) {
     return Expanded(
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           primary: color,
         ),
         onPressed: () {
-          playSound('$catMood.wav');
+          playSound('$catMood.$extension');
         },
         child: Image.asset('images/$catMood.png'),
       ),
@@ -35,50 +53,48 @@ class CatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xffF0997D),
-        appBar: AppBar(
-          title: Text('Choose a cat'),
-          backgroundColor: Color(0xffA75D5D),
-        ),
-        body: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  buildCat('angry', kImgColorDark),
-                  buildCat('purring', kImgColorLight),
-                ],
-              ),
+    return Scaffold(
+      backgroundColor: Color(0xffF0997D),
+      appBar: AppBar(
+        title: Text('Choose a cat'),
+        backgroundColor: Color(0xffA75D5D),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                buildCat('angry', kImgColorDark, 'wav'),
+                buildCat('purring', kImgColorLight, 'wav'),
+              ],
             ),
-            Expanded(
-              child: Row(
-                children: [
-                  buildCat('sad', kImgColorLight),
-                  buildCat('puzzled', kImgColorDark),
-                ],
-              ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                buildCat('sad', kImgColorLight, 'wav'),
+                buildCat('puzzled', kImgColorDark, 'mp3'),
+              ],
             ),
-            Expanded(
-              child: Row(
-                children: [
-                  buildCat('short_meow', kImgColorDark),
-                  buildCat('surprised', kImgColorLight),
-                ],
-              ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                buildCat('short_meow', kImgColorDark, 'wav'),
+                buildCat('surprised', kImgColorLight, 'wav'),
+              ],
             ),
-            Expanded(
-              child: Row(
-                children: [
-                  buildCat('upset', kImgColorLight),
-                  buildCat('cat_food', kImgColorDark),
-                ],
-              ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                buildCat('upset', kImgColorLight, 'wav'),
+                buildCat('cat_food', kImgColorDark, 'wav'),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
